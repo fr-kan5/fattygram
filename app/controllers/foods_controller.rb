34a1b8b3ce612f35b93557ba67c1c67e29eb2a35
fr-kan5 <manwith1713@gmail.com>
2021-03-04 11:16:11 +0900

    コントローラーへ投稿用の記述

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -9,6 +9,18 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+    @food.save
+    redirect_to food_path(@food)
+  end
+
   def edit
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:title, :body, :image)
   end
 end
